4eca78ce80146cf29989dc5d6eedcf5422b65e28
AymenLaroussi <aymen.laroussi@live.fr>
2022-02-14 21:32:24 +0100

    Affecter les crud à l'entité  catégorie

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProduitController extends AbstractController
+class ProduitsController extends AbstractController
 {
     /**
-     * @Route("/produit", name="produit")
+     * @Route("/produits", name="produits")
      */
     public function index(): Response
     {
-        return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+        return $this->render('produits/index.html.twig', [
+            'controller_name' => 'ProduitsController',
         ]);
     }
 }
