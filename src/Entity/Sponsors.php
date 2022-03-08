0534e05788a0f2637ceca047c48d44cc7f7fd233
aychasalhi <aicha.salhi@esprit.tn>
2022-03-08 12:33:18 +0100

    COMMITFINAL

--- src/Entity/Sponsors.php
+++ src/Entity/Sponsors.php
@@ -26,11 +26,6 @@ class Sponsors
      */
     private $nom;
 
-   /**
-     * @ORM\Column(type="string", length=100)
-     * @Assert\NotBlank(message="Le champ est obligatoire")
-     */
-    private $prenom;
 
     /**
      * @ORM\Column(type="integer")
@@ -72,18 +67,6 @@ class Sponsors
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
 
         return $this;
     }
